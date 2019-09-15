e4047658c4dd717f3d766c8c86d798937e799ad2
shoun <shooouuun@gmail.com>
2019-09-15 18:06:11 +0300

    fix for travis

--- js/stat.js
+++ js/stat.js
@@ -26,16 +26,16 @@ window.renderStatistics = function (ctx, names, times) {
   renderCloud(dataCloud.coordinatesCloud[0], dataCloud.coordinatesCloud[1], dataCloud.widthRect, dataCloud.heightRect, dataCloud.colorRect[1]);
   renderText(dataCloud.text);
 
-  function renderCloud(X, Y, width, height, colorRect) {  // рендеринг холста с подставкой параметров
+  function renderCloud(X, Y, width, height, colorRect) {// рендеринг холста с подставкой параметров
     ctx.fillStyle = colorRect;
     ctx.fillRect(X, Y, width, height);
   }
 
-  function renderText(textArray) {  // рендеринг текста
+  function renderText(textArray) {// рендеринг текста
     ctx.fillStyle = dataCloud.colorText;
     ctx.font = dataCloud.fontText;
 
-    for (var i = 0; i < textArray.length; i++) {  // смещает каждый последующий текст в массиве вниз
+    for (var i = 0; i < textArray.length; i++) {// смещает каждый последующий текст в массиве вниз
       var gapY = i ? 25 : 30;
       ctx.fillText(textArray[i], dataCloud.coordinatesCloud[0] + dataCloud.marginX, dataCloud.coordinatesCloud[1] + (i + 1) * gapY);
     }
@@ -47,15 +47,15 @@ window.renderStatistics = function (ctx, names, times) {
     шкалы результатов в внутреннем цикле(*) исходя из полученных данных корневого цикла.
   */
   for (var i = 0; i < names.length; i++) {
-    var maxTime = Math.max.apply(null, times);  // переменная с максимальным значением из массива времени
+    var maxTime = Math.max.apply(null, times);// переменная с максимальным значением из массива времени
 
-    var getRandom = function (min, max) {  // функция вернёт рандомные параметры на основе введённых значений
+    var getRandom = function (min, max) {// функция вернёт рандомные параметры на основе введённых значений
       var rand = min - 0.5 + Math.random() * (max - min + 1);
       rand = Math.round(rand);
       return rand;
     };
 
-    for (var j = 0; j < names.length; j++) {  // цикл рендерит шкалу результатов, время и имена*
+    for (var j = 0; j < names.length; j++) {// цикл рендерит шкалу результатов, время и имена*
       var randomColor = 'rgb( 0, 0, ' + getRandom(50, 255) + ')';
       ctx.fillStyle = '#000';
       ctx.fillText(names[j], dataCloud.textX + (dataCloud.widthBar + dataCloud.gap) * j, dataCloud.textY);
