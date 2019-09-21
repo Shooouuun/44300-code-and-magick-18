8aa56b0545f6da8d3b44f3e3c503c5fbdb1f33a1
shoun <shooouuun@gmail.com>
2019-09-21 14:19:45 +0300

    Задача: Нас орда - выполнена.

---
+++ js/setup.js
@@ -0,0 +1,99 @@
+'use strict';
+
+let setup = document.querySelector('.setup');
+setup.classList.remove('hidden');
+
+let setupSimilar = document.querySelector('.setup-similar');
+setupSimilar.classList.remove('hidden');
+
+let similarListElement = setupSimilar.querySelector('.setup-similar-list');
+let similarWizardTemplate = document.querySelector('#similar-wizard-template')
+  .content.querySelector('.setup-similar-item');
+
+const NAMES = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон'
+];
+
+const LAST_NAMES = [
+  'да Марья',
+  'Верон',
+  'Мирабелла',
+  'Вальц',
+  'Онопко',
+  'Топольницкая',
+  'Нионго',
+  'Ирвинг'
+];
+
+const COAT_COLORS = [
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'
+];
+
+const EYES_COLORS = [
+  'black',
+  'red',
+  'blue',
+  'yellow',
+  'green'
+];
+
+const FIREBALL_COLORS = [
+  '#ee4830',
+  '#30a8ee',
+  '#5ce6c0',
+  '#e848d5',
+  '#e6e848'
+];
+
+function getRandomElement (arr) {
+  return arr[Math.floor(arr.length * Math.random())];
+};
+
+// Генерация блока похожих персонажей
+
+function createWizards (num) {
+  let wizards = [];
+
+  for (let i = 0; i < num; i++) {
+    wizards[i] = {
+      name: getRandomElement(NAMES) + ' ' + getRandomElement(LAST_NAMES),
+      coatColor: getRandomElement(COAT_COLORS),
+      eyesColor: getRandomElement(EYES_COLORS)
+    };
+  }
+  return wizards;
+};
+
+function renderWizard (wizard) {
+  let wizardElement = similarWizardTemplate.cloneNode(true);
+
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+
+  return wizardElement;
+};
+
+function renderAllWizards (wizards) {
+  let fragment = document.createDocumentFragment();
+
+  for (let i = 0; i < wizards.length; i++) {
+    fragment.appendChild(renderWizard(wizards[i]));
+  }
+
+  return fragment;
+};
+
+similarListElement.appendChild(renderAllWizards(createWizards(4)));
